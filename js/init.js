c857c1903636659fc99fa28722b022dd9513287c
chat3cv51ASV <94591418+chat3cv51ASV@users.noreply.github.com>
2021-11-17 23:18:36 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDK1m8_4SoknHklybBj8S7J17v60njWx4o",
+  authDomain: "proyect-autentifica.firebaseapp.com",
+  projectId: "proyect-autentifica",
+  storageBucket: "proyect-autentifica.appspot.com",
+  messagingSenderId: "724776870122",
+  appId: "1:724776870122:web:1b17e502dc1b0973c93793",
+  measurementId: "G-E202BWDJHT"
+    });
