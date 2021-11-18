a36e8eb9fdc8465123b696570a2b34b914717372
chat3cv51ASV <94591418+chat3cv51ASV@users.noreply.github.com>
2021-11-17 23:22:56 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDK1m8_4SoknHklybBj8S7J17v60njWx4o",
-  authDomain: "proyect-autentifica.firebaseapp.com",
-  projectId: "proyect-autentifica",
-  storageBucket: "proyect-autentifica.appspot.com",
-  messagingSenderId: "724776870122",
-  appId: "1:724776870122:web:1b17e502dc1b0973c93793",
-  measurementId: "G-E202BWDJHT"
+   apiKey: "AIzaSyB978n7mZfvnEIwGNyVWu4seIHljUZeiR0",
+  authDomain: "proyect-chat.firebaseapp.com",
+  projectId: "proyect-chat",
+  storageBucket: "proyect-chat.appspot.com",
+  messagingSenderId: "471679598023",
+  appId: "1:471679598023:web:483561956f3f933b2a6de2",
+  measurementId: "G-BTPWN1T5C8"
     });
